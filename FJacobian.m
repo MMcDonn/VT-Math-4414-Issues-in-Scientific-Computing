44d8bae21388eb6c7c02ec186fee7625b57c4c32
Michael McD <mmicha2@vt.edu>
2016-01-18 11:45:53 -0500

    remove FJacobian.m

--- FJacobian.m
+++
@@ -1,7 +0,0 @@
-function J = FJacobian(x)
-n = max(size(x));
-J = zeros(n,n);
-J(1,1) = 3; J(1,2) = x(3)*sin(x(2)*x(3)); J(1,3) = x(2)*sin(x(2)*x(3));
-J(2,1) = 8*x(1); J(2,2) = 2 - 1250*x(2);
-J(3,1) = -exp(-x(1)*x(2))*x(2); J(3,2) = -exp(-x(1)*x(2))*x(1); J(3,3) = 20;
-end
